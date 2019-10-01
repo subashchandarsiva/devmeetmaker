5e9223e02ca0b295439992c63d490c624cdaa67b
subashchandarsiva <subashchandarsiva@gmail.com>
2019-10-01 16:35:52 +0530

    setting initial routes

--- server.js
+++ server.js
@@ -10,4 +10,11 @@ app.get("/", (req, res) => {
   res.send("hi Devs");
 });
 
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+app.use("/api/auth", require("./routes/api/auth"));
+
+app.use("/api/post", require("./routes/api/post"));
+
 app.listen(PORT, () => console.log(`App running in ${PORT}`));
